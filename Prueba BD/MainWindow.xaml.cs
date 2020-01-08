08fa027c2f2c06e06d16b4aba456a90c4026c099
AitorSoto <aitor.soto@iesdoctorbalmis.com>
2020-01-08 13:36:06 +0100

    Agregar archivos de proyecto.

---
+++ Prueba BD/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prueba_BD
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SQL_Database_AitorEntities context;
+        public MainWindow()
+        {
+            InitializeComponent();
+            context = new SQL_Database_AitorEntities();
+            //context.CLIENTES.Load();
+            //clientesListBox.DataContext = context.CLIENTES.Local;
+
+            var consulta = from    n in context.CLIENTES
+                           where   n.genero == "MALE"
+                           orderby n.nombre
+                           select  n;
+            clientesListBox.DataContext = new ObservableCollection<CLIENTE>(consulta.ToList());
+        }
+    }
+}
